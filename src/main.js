ff17877dcd8b9de0f08df17646353c3e967390aa
tangmu41 <1401016567@qq.com>
2022-03-16 22:31:20 +0800

    引入element自定义主题

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
+import '../theme/index.css'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
